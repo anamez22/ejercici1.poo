1ebcf9960ada4f59541540f8e1778cbf41c6f361
anamez22 <ana.amezquita@correounivalle.edu.co>
2024-09-18 11:01:35 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+num01 = int(input("ingrese el primer numero entero: "))
+num02 = int(input("ingrese el segundo numero entero: "))
+
+suma = num01 + num02
+resta = num01 - num02
+multiplicacion = num01 * num02
+division = num01 / num02 
+
+print(f"Suma: {suma}, Resta: {resta}, Multiplicacion: {multiplicacion}, Division: {division}")
